1b05e3576b9cf47e5e3836b4b50a0ac7f15a4deb
sikiru <sikirurazak1@gmail.com>
2025-08-03 06:23:58 +0100

    chore: add spring framework for IOC

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,6 +3,8 @@ package com.example;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -12,17 +14,22 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        // User user = new User();
-        // user.setFirstName("Mary");
-        // user.setLastName("Doe");
-        // user.setEmail("mary.doe@example.com");
-
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         SessionFactory sFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(com.example.User.class).buildSessionFactory();
-
         Session session = sFactory.openSession();
+        System.out.println( "Hello World!" );
+        User obj = context.getBean("user", User.class);
+        obj.setFirstName("Mary");
+        obj.setLastName("Doe");
+        obj.setEmail("mary.doe1@example.com");
+        session.beginTransaction();
+        session.persist(obj);
+        session.getTransaction().commit();
+
+        
+
         User user = session.find(User.class, 1);
-        user.setEmail("john.doe@example.com");
+        user.setEmail("john.doe1@example.com");
         session.beginTransaction();
         session.merge(user);
         session.getTransaction().commit();
